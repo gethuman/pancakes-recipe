a509c1abacd7d9899c02886fa4b479d2b7f89e8f
Jeff Whelpley <jeff@gethuman.com>
2015-02-04 10:09:24 -0500

    Updating client side config to use right SSL value

--- middleware/mw.view.model.js
+++ middleware/mw.view.model.js
@@ -7,9 +7,15 @@
  */
 module.exports = function (_, appConfigs, i18n, config) {
 
-    var useSSL = (config.api && config.api.useSSL !== undefined) ?
-        config.api.useSSL : config.useSSL;
-    var httpPrefix = useSSL ? 'https://' : 'http://';
+    var useSSL = (config.api && config.api.clientUseSSL !== undefined) ?
+        config.api.clientUseSSL : config.useSSL;
+    var apiBase = (useSSL ? 'https://' : 'http://') + config.api.host;
+    var apiPort = config.api.port;
+
+    if (apiPort !== 80 && apiPort !== 443) {
+        apiBase += ':' + apiPort;
+    }
+    apiBase += '/' + config.api.version;
 
     /**
      * Add to the model with certain custom values for the app
@@ -20,10 +26,12 @@ module.exports = function (_, appConfigs, i18n, config) {
     function addToModel(model, routeInfo) {
         model = model || {};
 
-        var appName      = model.appName = routeInfo.appName;
-        var appConfig    = appConfigs[appName];
-        var appPascal    = appName.substring(0, 1).toUpperCase() + appName.substring(1);
-        var apiBase      = httpPrefix + config.api.host + ':' + config.api.port + '/' + config.api.version;
+        var appName     = model.appName = routeInfo.appName;
+        var appConfig   = appConfigs[appName];
+        var appPascal   = appName.substring(0, 1).toUpperCase() + appName.substring(1);
+        var staticSSL   = (config[appName] && config[appName].useSSL !== undefined) ?
+            config[appName].useSSL : config.useSSL;
+        var staticFileRoot = (staticSSL ? 'https://' : 'http://') + config.staticFiles.assets + '/';
 
         // add the page head info
         if (model.pageHead) {
@@ -58,7 +66,7 @@ module.exports = function (_, appConfigs, i18n, config) {
                 appEnv:                 config.env,
                 version:                config.staticVersion,
                 baseHost:               config.baseHost,
-                staticFileRoot:         httpPrefix + config.staticFiles.assets + '/',
+                staticFileRoot:         staticFileRoot,
                 domains:                config.domains,
                 realtime: {
                     postsHost:          config.realtime.postsHost,
