f7d9347287d219acc957487da4ee50472eb9b026
Jeff Whelpley <jeff@gethuman.com>
2015-05-01 15:17:42 -0400

    Removing old slugify code that is not used anymore

--- utils/slugify.js
+++ utils/slugify.js
@@ -16,18 +16,7 @@ module.exports = function () {
      */
     return function (val) {
         val = val || '';
-        // "replace 1+ non-alphanumerics with - and then trim - from the start and end"
         return val.replace(/[^a-zA-Z0-9]+/g, '-').replace(/^\-|\-$/g, '');
-        /*
-        return val
-            .replace(/\//g, '-')
-            .replace(/[^a-zA-Z0-9\- ]/g, '')
-            .trim()
-            .replace(/ /g, '-')
-            .replace(/--/g, '-');
-            */
     };
-
-    // CA: propose changing to: return val.replace(/[^a-zA-Z0-9]+/g, '-').replace(/^\-|\-$/g, '');
 };
 
