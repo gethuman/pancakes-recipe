20f647db08dafc4cbdd37d953870b88c7f7e6e6a
Jeff Whelpley <jeff@gethuman.com>
2015-07-06 08:29:46 -0400

    Updating deps

--- utils/hash.js
+++ utils/hash.js
@@ -12,7 +12,7 @@ module.exports = function (Q, bcrypt) {
      * @param data
      * @param encrypted
      */
-    var compare = function (data, encrypted) {
+    function compare(data, encrypted) {
         var deferred = Q.defer();
 
         bcrypt.compare(data, encrypted, function (err, res) {
@@ -20,13 +20,13 @@ module.exports = function (Q, bcrypt) {
         });
 
         return deferred.promise;
-    };
+    }
 
     /**
      * Generate hash off some data
      * @param data
      */
-    var generateHash = function (data) {
+    function generateHash(data) {
         var deferred = Q.defer();
 
         bcrypt.hash(data, 10, function (err, res) {
@@ -34,11 +34,26 @@ module.exports = function (Q, bcrypt) {
         });
 
         return deferred.promise;
-    };
+    }
+
+    //TODO: potentially consolidate generateHash and md5Hash in the future
+
+    /**
+     * Used by actionItemService to create hash for action items
+     * @param val
+     * @returns {*}
+     */
+    function md5Hash(val) {
+        var str = _.isObject(val) ? JSON.stringify(val) : val;
+        var md5 = crypto.createHash('md5');
+        md5.update(str);
+        return md5.digest('hex');
+    }
 
     // expose functions
     return {
         compare: compare,
-        generateHash: generateHash
+        generateHash: generateHash,
+        md5Hash: md5Hash
     };
 };
