39551e6007c0441bd1061222c31b8269aa43dc9f
Christian Allen <christian@christianallen.com>
2015-10-26 23:58:55 -0400

    more redirects to overcome huge numbers of 404 errors

--- middleware/mw.invalid.path.js
+++ middleware/mw.invalid.path.js
@@ -6,16 +6,15 @@
  * Short curcuit invalid paths up front
  */
 module.exports = function (Q) {
-    var invalidSuffixes = /(\.php|\.cgi|\.aspx)$/;
-    var invalidPrefixes = /^\/(cgi-bin|wp-admin|wp-content|wp-include)\//;
+    var invalidSuffixes = /(\.php|\.cgi|\.aspx|\.asp)$/;
+    var invalidPrefixes = /^\/(cgi-bin|wp|wp-admin|wp-content|wp-include|wp-includes|wordpress)\//;
+    var deprecatedPrefixes = /^\/(video)\//;
     var invalidRss = /(\/rss|\/atom)/;
     var invalidImages = /^\/(images)\//;
     var invalidCss = /^\/(css)\//;
     var invalidPaths = [
         '/crossdomain.xml',
-        '/browserconfig.xml',
         '/favicon.gif',
-        '/urlEmail',
         '/atom.xml',
         '/match'
     ];
@@ -26,7 +25,7 @@ module.exports = function (Q) {
                 var url = request.url.pathname.toLowerCase();
 
                 // 404 if ends with php, asp, aspx
-                if (invalidPrefixes.test(url) || invalidSuffixes.test(url)) {
+                if (invalidPrefixes.test(url) || invalidSuffixes.test(url) || deprecatedPrefixes.test(url)) {
                     // or... we could just redirect to home page... why not?
                     //reply('Invalid path').code(404);
                     reply().redirect('/').permanent(true);
@@ -47,8 +46,27 @@ module.exports = function (Q) {
                 }
 
                 if (url.substring(0, 8) === '/cgi-bin') {
-                    reply('Invalid path').code(404);
-                    return;
+                    return reply('Invalid path').code(404);
+                }
+
+                // todo: ask Jeff how to get these back to gh- don't belong here...
+                if (/\/problemsUrl$/i.test(url)) {
+                    return reply().redirect('https://problems.gethuman.com').permanent(true);
+                }
+                if (/\/answersUrl$/i.test(url)) {
+                    return reply().redirect('https://answers.gethuman.com').permanent(true);
+                }
+                if (/\/reviewsUrl$/i.test(url)) {
+                    return reply().redirect('https://reviews.gethuman.com').permanent(true);
+                }
+                if (/\/myUrl$/i.test(url)) {
+                    return reply().redirect('https://my.gethuman.com').permanent(true);
+                }
+                if (/\/iUrlTeam$/i.test(url)) {
+                    return reply().redirect('https://i.gethuman.com/team').permanent(true);
+                }
+                if (/\/iUrlLegal$/i.test(url)) {
+                    return reply().redirect('https://i.gethuman.com/legal').permanent(true);
                 }
 
                 // if we get here, we can continue as normal
