49dc8a97e93ff2103e001ce19c28915d5a4ec9bf
Jeff Whelpley <jeff@gethuman.com>
2016-04-14 05:24:13 -0400

    Updating deps

--- pancakes.recipe.js
+++ pancakes.recipe.js
@@ -8,9 +8,9 @@
 var tastePancakes       = require('./test/pancakes.taste');
 var RedisAdapter        = require('./services/adapters/redis/redis.adapter');
 var ApiclientAdapter    = require('./services/adapters/apiclient/apiclient.adapter');
-var TranslatorAdapter   = require('./services/adapters/translator/translator.adapter');
+// var TranslatorAdapter   = require('./services/adapters/translator/translator.adapter');
 var logReactor          = require('./services/reactors/log.reactor');
-var translationReactor  = require('./services/reactors/translation.reactor');
+// var translationReactor  = require('./services/reactors/translation.reactor');
 
 module.exports = {
     rootDir:            __dirname,
@@ -18,12 +18,12 @@ module.exports = {
     serverModuleDirs:   ['middleware', 'utils', 'batch'],
     adapters: {
         redis:          RedisAdapter,
-        apiclient:      ApiclientAdapter,
-        translator:     TranslatorAdapter
+        apiclient:      ApiclientAdapter
+        // translator:     TranslatorAdapter
     },
     reactors: {
-        log:            logReactor,
-        translation:    translationReactor
+        log:            logReactor
+        // translation:    translationReactor
     }
 };
 
